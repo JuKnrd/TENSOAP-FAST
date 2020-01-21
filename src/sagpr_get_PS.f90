cf01dee054eb40bc93e9fad7de1138b64e642841
dilkins <wilkinsd@outlook.com>
2020-01-21 17:46:52 +0100

    Power spectrum building started

--- src/sagpr_get_PS.f90
+++ src/sagpr_get_PS.f90
@@ -4,13 +4,16 @@ program sagpr_predict
 
     integer nargs,numkeys
     character(len=100), allocatable :: arg(:),keylist(:)
-    integer lm,i,j,nmol,reals,bytes,degen,nenv,mu,nu,k
+    integer lm,i,j,nmol,reals,bytes,degen,nenv,mu,nu,k,ii
     integer nmax,lmax,ncut
     real*8 rcut,sg,rs(3)
-    character(len=100) weights,ofile,kerfile,cen,spec,sparse,fname
+    character(len=100) weights,ofile,kerfile,sparse,fname
     real*8, allocatable, target :: all_wt(:),raw_ker(:)
     real*8, allocatable :: wt(:),prediction_lm(:,:),ker(:,:,:,:)
+    logical periodic,readnext
     real*8 meanval
+    integer, parameter :: nelements = 200
+    logical all_species(nelements),all_centres(nelements)
 
     ! Get input arguments
     nargs = iargc()
@@ -26,83 +29,67 @@ program sagpr_predict
     lmax = 6
     rcut = 4.d0
     sg = 0.3d0
-    cen = ''
-    spec = ''
+    all_centres(:) = .false.
+    all_species(:) = .false.
     ncut = -1
     sparse = ''
     fname = ''
     rs = (/0.d0,0.d0,0.d0/)
     ofile = ''
-    keylist = (/'-lm ','-n  ','-l  ','-rc ','-sg ','-c  ','-s  ','-nc ','-sf ','-f  ','-rs ','-o  ','NULL'/)
-
-    stop
-
-    lm = 0
-    weights = ''
-    kerfile = ''
-    ofile = 'prediction.out'
-    numkeys = 5
+    periodic = .false.
+    numkeys = 14
     allocate(keylist(numkeys))
-    keylist = (/'-lm ','-w  ','-k  ','-o  ','NULL'/)
+    keylist = (/'-lm ','-n  ','-l  ','-rc ','-sg ','-c  ','-s  ','-nc ','-sf ','-f  ','-rs ','-o  ','-p  ','NULL'/)
     do i=1,nargs
      arg(i) = trim(adjustl(arg(i)))
      if (arg(i).eq.'-lm') read(arg(i+1),*) lm
-     if (arg(i).eq.'-w') read(arg(i+1),*) weights
-     if (arg(i).eq.'-k') read(arg(i+1),*) kerfile
+     if (arg(i).eq.'-n') read(arg(i+1),*) nmax
+     if (arg(i).eq.'-l') read(arg(i+1),*) lmax
+     if (arg(i).eq.'-rc') read(arg(i+1),*) rcut
+     if (arg(i).eq.'-sg') read(arg(i+1),*) sg
+     if (arg(i).eq.'-c') then
+      readnext = .true.
+      do k=i+1,nargs
+       do j=1,numkeys
+        readnext = readnext.and.(trim(adjustl(arg(k))).ne.trim(adjustl(keylist(j))))
+       enddo
+       if (readnext) then
+        read(arg(k),*) ii
+        all_centres(ii) = .true.
+       endif
+      enddo
+     endif
+     if (arg(i).eq.'-s') then
+      readnext = .true.
+      do k=i+1,nargs
+       do j=1,numkeys
+        readnext = readnext.and.(trim(adjustl(arg(k))).ne.trim(adjustl(keylist(j))))
+       enddo
+       if (readnext) then
+        read(arg(k),*) ii
+        all_species(ii) = .true.
+       endif
+      enddo
+     endif
+     if (arg(i).eq.'-nc') read(arg(i+1),*) ncut
+     if (arg(i).eq.'-sf') read(arg(i+1),*) sparse
+     if (arg(i).eq.'-f') read(arg(i+1),*) fname
+     if (arg(i).eq.'-rs') then
+      read(arg(i+1),*) rs(1)
+      read(arg(i+2),*) rs(2)
+      read(arg(i+3),*) rs(3)
+     endif
      if (arg(i).eq.'-o') read(arg(i+1),*) ofile
+     if (arg(i).eq.'-p') periodic=.true.
     enddo
 
     ! Check for arguments that are required
-    if (weights.eq.'') stop 'ERROR: weights file required!'
-    if (kerfile.eq.'') stop 'ERROR: kernel file required!'
+    if (fname.eq.'') stop 'ERROR: filename required!'
+    if (ofile.eq.'') stop 'ERROR: output file required!'
 
-    ! Read in weights
-    degen = 2*lm + 1
-    open(unit=31,file=weights,status='old',access='stream',form='unformatted')
-    inquire(unit=31,size=bytes)
-    reals = bytes/8
-    allocate(all_wt(reals),wt(reals-1))
-    read(31,pos=1) all_wt
-    meanval = all_wt(1)
-    do i=2,reals
-     wt(i-1) = all_wt(i)
-    enddo
-    nenv = (reals-1) / degen
-    close(31)
-
-    ! Read in kernel
-    open(unit=32,file=kerfile,status='old',access='stream',form='unformatted')
-    inquire(unit=32,size=bytes)
-    reals = bytes/8
-    allocate(raw_ker(reals))
-    read(32,pos=1) raw_ker
-    close(32)
-    nmol = reals / (nenv * degen * degen)
-    allocate(ker(nmol,nenv,degen,degen))
-    k = 0
-    do i=1,nmol
-     do j=1,nenv
-      do nu=1,degen
-       do mu=1,degen
-        k = k + 1
-        ker(i,j,mu,nu) = raw_ker(k)
-       enddo
-      enddo
-     enddo
-    enddo
-
-    ! Do prediction
-    allocate(prediction_lm(nmol,degen))
-    prediction_lm = do_prediction(ker,wt,meanval,degen,nmol,nenv)
-
-    ! Print out predictions
-    open(unit=33,file=ofile)
-    do i=1,nmol
-     write(33,*) (prediction_lm(i,mu),mu=1,degen)
-    enddo
-    close(33)
+    ! Read in XYZ file
 
     ! Array deallocation
-    deallocate(all_wt,wt,keylist,ker,prediction_lm,raw_ker)
+    !TODO: deallocation
 
 end program
