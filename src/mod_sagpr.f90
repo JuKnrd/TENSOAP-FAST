24576afb82a71724b93a062dea110cc39993bcd9
dilkins <wilkinsd@outlook.com>
2020-01-22 23:06:37 +0100

    orthoradint matches with soapfast

--- src/mod_sagpr.f90
+++ src/mod_sagpr.f90
@@ -400,10 +400,10 @@ module sagpr
   implicit none
 
    integer natoms,nspecies,nmax,lmax,nnmax,natmax,nsmax,iat,ncentype,icentype,icen,cen,n,ispe
-   integer ineigh,neigh,lval,im,mval,i,j,k,l,m
-   real*8 rcut2,rx,ry,rz,r2,rdist,cth,ph
+   integer ineigh,neigh,lval,im,mval,n1,n2,l,i,j,k,nn,m
+   real*8 rcut2,rx,ry,rz,r2,rdist,cth,ph,normfact,sigmafact
    complex*16 omega(natoms,nspecies,nmax,lmax+1,2*lmax+1),harmonic(natoms,nelements,lmax+1,2*lmax+1,nnmax)
-   real*8 radint(natoms,nspecies,nnmax,lmax+1,nmax),orthoradint(natoms,nspecies,nnmax,lmax+1,nmax)
+   real*8 radint(natoms,nspecies,nnmax,lmax+1,nmax),orthoradint(natoms,nspecies,lmax+1,nmax,nnmax)
    real*8 efact(natoms,nelements,nnmax),length(natoms,nelements,nnmax),cell(3,3),rs(3),sg,rcut,xyz(natmax,3)
    real*8 sigma(nmax),orthomatrix(nmax,nmax),alpha,sg2,radial_c,radial_r0,radial_m
    integer nneigh(natoms,nelements),all_indices(nsmax,natmax),nneighmax(nsmax)
@@ -460,7 +460,7 @@ module sagpr
             length(iat,ispe,n) = rdist
             cth = rz/rdist
             ph = datan2(ry,rx)
-            efact(iat,ispe,n) = dexp(-alpha*r2) !* radial_scaling(rdist,radial_c,radial_r0,radial_m)
+            efact(iat,ispe,n) = dexp(-alpha*r2) * radial_scaling(rdist,radial_c,radial_r0,radial_m)
             do lval=0,lmax
              do im=0,2*lval
               mval = im-lval
@@ -480,34 +480,67 @@ module sagpr
     enddo
 
    else
-   endif 
 
-!   do i=1,natoms
-!    do j=1,nelements
-!     if (j.eq.1 .or. j.eq.8) then
-!      do k=1,nnmax
-!       write(*,*) i,j,k,length(i,j,k)
-!      enddo
-!     endif
-!    enddo
-!   enddo
-!   stop
+    stop 'Periodic code not yet set up!'
 
+   endif
 
-   do i=1,natoms
-    do j=1,nelements
-     if (j.eq.1 .or. j.eq.8) then
-      do k=1,lmax+1
-       do l=1,2*lmax+1
-        do m=1,nnmax
-         write(*,*) i,j,k,l,m,real(harmonic(i,j,k,l,m)),imag(harmonic(i,j,k,l,m))
+   ! Get radial integral
+   do n1=1,nmax
+    n2 = n1-1
+    normfact = dsqrt(2.d0 / (gamma(1.5d0 + n2)*sigma(n1)**(3.d0 + 2.d0*n2)))
+    sigmafact = (sg2**2 + sg2*sigma(n1)**2)/sigma(n1)**2
+    do i=1,natoms
+     k = 0
+     do j=1,nelements
+      if (all_species(j)) then
+       k = k + 1
+       do nn=1,nnmax
+        do l=0,lmax
+        radint(i,k,nn,l+1,n1) = efact(i,j,nn) * 2.d0**(-0.5d0*(1.d0 + l-n2)) * &
+     &     (1.d0/sg2 + 1.d0/sigma(n1)**2)**(-0.5d0*(3.d0 + l+n2)) * &
+     &     (gamma(0.5d0*(3.d0+l+n2))/gamma(1.5d0+l)) * (length(i,j,nn)/sg2)**l * &
+     &     hg(0.5d0*(3.d0+l+n2),1.5d0 + l,0.5d0 * length(i,j,nn)**2/sigmafact) 
         enddo
+       enddo
+      endif
+     enddo
+    enddo
+    radint(:,:,:,:,n1) = radint(:,:,:,:,n1) * normfact
+   enddo
+
+   ! Get orthoradint
+   do iat=1,natoms
+    k = 0
+    do j=1,nelements
+     if (all_species(j)) then
+      k = k + 1
+      do neigh=1,nneigh(iat,j)
+       do l=1,lmax+1
+        orthoradint(iat,k,l,:,neigh) = matmul(orthomatrix,radint(iat,k,neigh,l,:))
        enddo
       enddo
      endif
     enddo
    enddo
+
+   do i=1,natoms
+    do j=1,nspecies
+     do k=1,lmax+1
+      do l=1,nmax
+       do m=1,nnmax
+        write(*,*) i,j,k,l,m,orthoradint(i,j,k,l,m)   ! THIS IS ALWAYS ZERO (EVEN WHEN IT SHOULD NOT BE) WHEN j=2
+       enddo
+      enddo
+     enddo
+    enddo
+   enddo
    stop
+
+!      real*8 radint(natoms,nspecies,nnmax,lmax+1,nmax),orthoradint(natoms,nspecies,lmax+1,nmax,nnmax)
+!   real*8 efact(natoms,nelements,nnmax),length(natoms,nelements,nnmax),cell(3,3),rs(3),sg,rcut,xyz(nat
+
+   ! Get omega
 
  end subroutine
 
@@ -611,4 +644,191 @@ module sagpr
  end function
 
 !***************************************************************************************************
+
+ real*8 function hg ( a, b, x)
+
+!*****************************************************************************80
+!
+!! CHGM computes the confluent hypergeometric function M(a,b,x).
+!
+!  Licensing:
+!
+!    This routine is copyrighted by Shanjie Zhang and Jianming Jin.  However, 
+!    they give permission to incorporate this routine into a user program 
+!    provided that the copyright is acknowledged.
+!
+!  Modified:
+!
+!    27 July 2012
+!
+!  Author:
+!
+!    Shanjie Zhang, Jianming Jin
+!
+!  Reference:
+!
+!    Shanjie Zhang, Jianming Jin,
+!    Computation of Special Functions,
+!    Wiley, 1996,
+!    ISBN: 0-471-11963-6,
+!    LC: QA351.C45.
+!
+!  Parameters:
+!
+!    Input, real ( kind = 8 ) A, B, parameters.
+!
+!    Input, real ( kind = 8 ) X, the argument.
+!
+!    Output, real ( kind = 8 ) HG, the value of M(a,b,x).
+!
+  implicit none
+
+  real ( kind = 8 ) a
+  real ( kind = 8 ) a0
+  real ( kind = 8 ) a1
+  real ( kind = 8 ) b
+  real ( kind = 8 ) hg1
+  real ( kind = 8 ) hg2
+  integer ( kind = 4 ) i
+  integer ( kind = 4 ) j
+  integer ( kind = 4 ) k
+  integer ( kind = 4 ) la
+  integer ( kind = 4 ) m
+  integer ( kind = 4 ) n
+  integer ( kind = 4 ) nl
+  real ( kind = 8 ) pi
+  real ( kind = 8 ) r
+  real ( kind = 8 ) r1
+  real ( kind = 8 ) r2
+  real ( kind = 8 ) rg
+  real ( kind = 8 ) sum1
+  real ( kind = 8 ) sum2
+  real ( kind = 8 ) ta
+  real ( kind = 8 ) tb
+  real ( kind = 8 ) tba
+  real ( kind = 8 ) x
+  real ( kind = 8 ) x0
+  real ( kind = 8 ) xg
+  real ( kind = 8 ) y0
+  real ( kind = 8 ) y1
+
+  pi = dacos(-1.d0)
+  a0 = a
+  a1 = a
+  x0 = x
+  hg = 0.0D+00
+
+  if ( b == 0.0D+00 .or. b == - abs ( int ( b ) ) ) then
+    hg = 1.0D+300
+  else if ( a == 0.0D+00 .or. x == 0.0D+00 ) then
+    hg = 1.0D+00
+  else if ( a == -1.0D+00 ) then
+    hg = 1.0D+00 - x / b
+  else if ( a == b ) then
+    hg = exp ( x )
+  else if ( a - b == 1.0D+00 ) then
+    hg = ( 1.0D+00 + x / b ) * exp ( x )
+  else if ( a == 1.0D+00 .and. b == 2.0D+00 ) then
+    hg = ( exp ( x ) - 1.0D+00 ) / x
+  else if ( a == int ( a ) .and. a < 0.0D+00 ) then
+    m = int ( - a )
+    r = 1.0D+00
+    hg = 1.0D+00
+    do k = 1, m
+      r = r * ( a + k - 1.0D+00 ) / k / ( b + k - 1.0D+00 ) * x
+      hg = hg + r
+    end do
+  end if
+
+  if ( hg /= 0.0D+00 ) then
+    return
+  end if
+
+  if ( x < 0.0D+00 ) then
+    a = b - a
+    a0 = a
+    x = abs ( x )
+  end if
+
+  if ( a < 2.0D+00 ) then
+    nl = 0
+  end if
+
+  if ( 2.0D+00 <= a ) then
+    nl = 1
+    la = int ( a )
+    a = a - la - 1.0D+00
+  end if
+
+  do n = 0, nl
+
+    if ( 2.0D+00 <= a0 ) then
+      a = a + 1.0D+00
+    end if
+
+    if ( x <= 30.0D+00 + abs ( b ) .or. a < 0.0D+00 ) then
+
+      hg = 1.0D+00
+      rg = 1.0D+00
+      do j = 1, 500
+        rg = rg * ( a + j - 1.0D+00 ) &
+          / ( j * ( b + j - 1.0D+00 ) ) * x
+        hg = hg + rg
+        if ( abs ( rg / hg ) < 1.0D-15 ) then
+          exit
+        end if
+      end do
+
+    else
+
+      ta = gamma(a)
+      tb = gamma(b)
+      xg = b - a
+      tba = gamma(xg)
+      sum1 = 1.0D+00
+      sum2 = 1.0D+00
+      r1 = 1.0D+00
+      r2 = 1.0D+00
+      do i = 1, 8
+        r1 = - r1 * ( a + i - 1.0D+00 ) * ( a - b + i ) / ( x * i )
+        r2 = - r2 * ( b - a + i - 1.0D+00 ) * ( a - i ) / ( x * i )
+        sum1 = sum1 + r1
+        sum2 = sum2 + r2
+      end do
+      hg1 = tb / tba * x ** ( - a ) * cos ( pi * a ) * sum1
+      hg2 = tb / ta * exp ( x ) * x ** ( a - b ) * sum2
+      hg = hg1 + hg2
+
+    end if
+
+    if ( n == 0 ) then
+      y0 = hg
+    else if ( n == 1 ) then
+      y1 = hg
+    end if
+
+  end do
+
+  if ( 2.0D+00 <= a0 ) then
+    do i = 1, la - 1
+      hg = ( ( 2.0D+00 * a - b + x ) * y1 + ( b - a ) * y0 ) / a
+      y0 = y1
+      y1 = hg
+      a = a + 1.0D+00
+    end do
+  end if
+
+  if ( x0 < 0.0D+00 ) then
+    hg = hg * exp ( x0 )
+  end if
+
+  a = a1
+  x = x0
+
+  return
+ end function
+
+
+!***************************************************************************************************
+
 end module
