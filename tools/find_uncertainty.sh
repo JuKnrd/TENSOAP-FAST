da9ad6937d411acfdda880bcadc2a7e597376ea3
dilkins <wilkinsd@outlook.com>
2020-07-31 01:50:10 +0200

    Uncertainty script works for any order

--- tools/find_uncertainty.sh
+++ tools/find_uncertainty.sh
@@ -5,4 +5,4 @@
 export skip=${2}
 if [ "${skip}" == "" ];then export skip=1;fi
 
-cat ${1} | awk '{m=n=0;for (i=1;i<=NF;i++){n++;m+=$i};m/=n;l=0;for (i=1;i<=NF;i++){l+=($i-m)**2};l/=(n-1);printf "%.16f %.16f\n",m,l**0.5}'
+cat ${1} | awk 'BEGIN{sk=ENVIRON["skip"]}{for (j=1;j<=sk;j++){m=n=0;for (i=j;i<=NF;i+=sk){n++;m+=$i};m/=n;l=0;for (i=j;i<=NF;i+=sk){l+=($i-m)**2};l/=(n-1);printf "%.16f %.16f ",m,l**0.5};printf "\n"}' | awk '{for (i=1;i<=NF;i+=2){printf "%.16f ",$i};for (i=2;i<=NF;i+=2){printf "%.16f ",$i};printf "\n"}'
