a37518d53cd9b68db9c7edbd690f907cb76339a2
David M. Wilkins <d.wilkins@qub.ac.uk>
2020-11-24 23:09:32 +0000

    Code updated; needs testing

--- models/bulk-water/README.txt
+++ models/bulk-water/README.txt
@@ -6,8 +6,14 @@ To use these models, see instructions below:
 
 1. mu-H2O:
 	# Get lambda=1 prediction
-	${TENSOAP}/bin/sagpr_apply_process -f ${FILE}.xyz -m ${TENSOAP}/models/bulk-water/mu-H2O.mdl -o dipole_mu1.out
+	${TENSOAP}/bin/sagpr_apply_process -f ${FILE}.xyz -m ${TENSOAP}/models/bulk-water/mu-H2O.mdl -o mu_L1.out
 	# Convert to prediction of polarization
-	${TENSOAP}/tools/spherical_to_cartesian.sh 1 dipole_mu1.out dipole_prediction.out
+	${TENSOAP}/tools/spherical_to_cartesian.sh 1 mu_L1.out dipole_prediction.out
 
 2. alpha-H2O:
+	# Get lambda=0 prediction
+	${TENSOAP}/bin/sagpr_apply_process -f ${FILE}.xyz -m ${TENSOAP}/models/bulk-water/alpha-H2O-0.mdl -o alpha_L0.out
+	# Get lambda=2 prediction
+	${TENSOAP}/bin/sagpr_apply_process -f ${FILE}.xyz -m ${TENSOAP}/models/bulk-water/alpha-H2O-2.mdl -o alpha_L2.out
+	# Convert to prediction of polarizability
+	${TENSOAP}/tools/spherical_to_cartesian.sh 2 alpha_L0.out alpha_L2.out alpha_prediction.out
--- tools/spherical_to_cartesian.sh
+++ tools/spherical_to_cartesian.sh
@@ -14,18 +14,26 @@ if [ ${lval} == 1 ];then
 elif [ ${lval} == 2 ];then
 	fname0=${2}
 	fname2=${3}
-	export degen=5
-	export ncommittee=$(head -n 1 ${fname0} | awk '{print NF/ENVIRON["degen"]}')
-	export ncommitte2=$(head -n 1 ${fname2} | awk '{print NF/ENVIRON["degen"]}')
+	export degen0=1
+	export degen2=5
+	export ncommittee=$(head -n 1 ${fname0} | awk '{print NF/ENVIRON["degen0"]}')
+	export ncommitte2=$(head -n 1 ${fname2} | awk '{print NF/ENVIRON["degen2"]}')
 	if [ ${ncommittee} -ne ${ncommitte2} ];then
 		echo "ERROR: different numbers of committees for two models"
 		echo ${ncommittee} ${ncommitte2}
 		exit
 	fi
+	for i in $(seq 1 ${ncommittee});do
+		export ii=${i}
+		cat ${fname0} | awk 'BEGIN{dg=ENVIRON["degen0"];ii=ENVIRON["ii"]}{i1=(ii-1)*dg + 1;i2=ii*dg;for (i=i1;i<=i2;i++){printf "%f ",$i};printf "\n"}' > MODEL_${fname0}_${i}
+		cat ${fname2} | awk 'BEGIN{dg=ENVIRON["degen2"];ii=ENVIRON["ii"]}{i1=(ii-1)*dg + 1;i2=ii*dg;for (i=i1;i<=i2;i++){printf "%f ",$i};printf "\n"}' > MODEL_${fname2}_${i}
+		paste MODEL_${fname0}_${i} MODEL_${fname2}_${i} | awk 'BEGIN{f1=(1./3.)**0.5;f2=(3./2.)**0.5}{a0=$1;a2m2=$2;a2m1=$3;a20=$4;a2p1=$5;a2p2=$6;axy=ayx=a2m2;ayz=azy=a2m1;axz=azx=a20;axx=f1*(-a0-a2p1 + f2*a2p2);ayy=f1*(-a0-a2p1 - f2*a2p2);azz=f1*(-a0 + 2*a2p1);printf "%f %f %f %f %f %f %f %f %f\n",axx,axy,axz,ayx,ayy,ayz,azx,azy,azz}'
+	done
 elif [ ${lval} == 0 ];then
 	echo "No conversion needed"
 	exit
 else
-	echo "This l value is not supporte"
+	echo "This l value is not supported"
 	exit
 fi
+#rm MODEL_*
