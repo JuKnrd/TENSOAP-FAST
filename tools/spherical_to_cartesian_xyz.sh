a52f98f5631af69396d05ec40f77db13a4921cd5
David Wilkins <d.wilkins@qub.ac.uk>
2021-05-17 09:44:45 +0100

    Atom name included in script output

--- tools/spherical_to_cartesian_xyz.sh
+++ tools/spherical_to_cartesian_xyz.sh
@@ -10,7 +10,7 @@ elif [ ${lval} == 2 ];then
 	fname0=${2}
 	fname2=${3}
 	oname=${4}
-	paste ${fname0} ${fname2} | awk 'BEGIN{f2=(1./2.)**0.5;f3=(1./3.)**0.5;f6=(1./6.)**0.5;f23=(2./3.)**0.5}/#/{print "#"}!/#/{if (NF==2){print $1}else{a0=$2;a2m2=$4;a2m1=$5;a20=$6;a2p1=$7;a2p2=$8;axy=ayx=f2*a2m2;ayz=azy=f2*a2m1;axz=azx=f2*a20;axx=(-f3*a0 - f6*a2p1 + f2*a2p2);ayy=(-f3*a0 - f6*a2p1 - f2*a2p2);azz=(-f3*a0 + f23*a2p1);printf "%f %f %f %f %f %f %f %f %f\n",axx,axy,axz,ayx,ayy,ayz,azx,azy,azz}}' > ${oname}
+	paste ${fname0} ${fname2} | awk 'BEGIN{f2=(1./2.)**0.5;f3=(1./3.)**0.5;f6=(1./6.)**0.5;f23=(2./3.)**0.5}/#/{print "#"}!/#/{if (NF==2){print $1}else{a0=$2;a2m2=$4;a2m1=$5;a20=$6;a2p1=$7;a2p2=$8;axy=ayx=f2*a2m2;ayz=azy=f2*a2m1;axz=azx=f2*a20;axx=(-f3*a0 - f6*a2p1 + f2*a2p2);ayy=(-f3*a0 - f6*a2p1 - f2*a2p2);azz=(-f3*a0 + f23*a2p1);printf "%s %f %f %f %f %f %f %f %f %f\n",$1,axx,axy,axz,ayx,ayy,ayz,azx,azy,azz}}' > ${oname}
 elif [ ${lval} == 0 ];then
 	echo "No conversion needed"
 	exit
